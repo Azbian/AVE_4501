e7f8eca73771c847600aca37cddaebb5a4069888
Azbian <mdneon255@gmail.com>
2024-11-14 10:16:48 +0600

    Report 7 and 8 added

--- Lab_07.m
+++ Lab_07.m
@@ -5,29 +5,27 @@ t=(-duration/2:1/Fs:duration/2);
 k1=2;
 k2=3;
 
+%Input signals
 x1= (t>=0)-((t-1)>=0);
 x2= sin(10*t).*(((t+1)>=0)-((t-2)>=0));
 
 figure;
-subplot(1,2,1);
 plot(t,x1);
 title("Signal x1");
 xlabel("Time (t)");
-ylabel("Amplitude");
+ylabel("Amplidtude");
 
-subplot(1,2,2);
+figure;
 plot(t,x2);
 title("Signal x2");
 xlabel("Time (t)");
-ylabel("Amplitude");
-
-x3=k1.*x1+k2.*x2;
+ylabel("Amplidtude");
 
 figure;
-plot(t,x3);
-title("Signal x3");
+plot(t,k1*x1+k2*x2);
+title("Signal k1*x1+k2*x2");
 xlabel("Time (t)");
-ylabel("Amplitude");
+ylabel("Amplidtude");
 
 function system_y1=y1(signal)
     system_y1=2.*signal;
@@ -38,43 +36,48 @@ function system_y2=y2(signal)
 end
 
 function system_y3=y3(signal)
-    system_y3=signal*exp(signal);
+    system_y3=signal.*exp(signal);
 end
 
-
-output1=y1(x3);
-output1_1=k1.*y1(x1);
-output1_2=k2.*y1(x2);
-
-figure;
-
-subplot(2,2,1);
-plot(t,output1_1);
-title("Signal k1*y1(x1)");
-xlabel("Time (t)");
-ylabel("Amplitude");
-
-subplot(2,2,2);
-plot(t,output1_2);
-title("Signal k2*y1(x2)");
-xlabel("Time (t)");
-ylabel("Amplitude");
-
-subplot(2,2,3);
-plot(t,output1);
-title("Signal y1(k1*x1+k2*x2)");
-xlabel("Time (t)");
-ylabel("Amplitude");
-
-subplot(2,2,4);
-plot(t,(output1_1+output1_2)-output1);
-title("Signal k1*y1(x1)+k2*y1(x2)");
-xlabel("Time (t)");
-ylabel("Amplitude");
-
-if ((output1_1+output1_2)-output1)==0
-    disp("System y1 is linear");
-else
-    disp("System y1 is non-linear")
+function linearity=if_linear(signal_1,signal_2,system,weights,t,system_name)
+    sum_of_weighted_inputs=weights(1).*signal_1+weights(2).*signal_2;
+    system_out_sum_of_weighted_inputs=system(sum_of_weighted_inputs);
+    weighted_output_signal_1=weights(1).*system(signal_1);
+    weighted_output_signal_2=weights(2).*system(signal_2);
+    figure;
+    subplot(1,2,1);
+    plot(t,system_out_sum_of_weighted_inputs);
+    title("y(k1*x1+k2+x2)")
+    xlabel("Time");
+    ylabel("Amplitude");
+    subplot(1,2,2);
+    plot(t,(weighted_output_signal_1+weighted_output_signal_2));
+    title("k1*y(x1)+k2*(x2)")
+    xlabel("Time");
+    ylabel("Amplitude");
+    linearity=(weighted_output_signal_1+weighted_output_signal_2)-system_out_sum_of_weighted_inputs;
+    if linearity==0
+        fprintf("System %s is linear\n",system_name);
+    else
+        fprintf("System %s is non-linear\n",system_name);
+    end
 end
+
+function system_ouput=system_output(signal,systems,t,system_names,signal_name)
+    figure;
+    for i=1:1:length(systems)
+        subplot(1,length(systems),i);
+        plot(t,systems{i}(signal));
+        title(sprintf("%s(%s)",system_names(i),signal_name));
+        xlabel("Time (t)");
+        ylabel("Amplitude");
+    end
+end
+
+system_output(x1,{@y1,@y2,@y3},t,["y1","y2","y3"],"x1");
+system_output(x2,{@y1,@y2,@y3},t,["y1","y2","y3"],"x2");
+
+if_linear(x1,x2,@y1,[k1,k2],t,"y1");
+if_linear(x1,x2,@y2,[k1,k2],t,"y2");
+if_linear(x1,x2,@y3,[k1,k2],t,"y3");
 
--- Lab_08.m
+++ Lab_08.m
@@ -2,56 +2,100 @@ Fs=100;
 duration=10;
 t=(-duration/2:1/Fs:duration/2);
 
-function system_y1=y1(signal)
-    system_y1=2.*signal;
-end
-
 function x1=signal_x1(time)
     x1=(time>=0)-((time-1)>=0);
 end
 
+function x2=signal_x2(time)
+    x2= sin(10*time).*(((time+1)>=0)-((time-2)>=0));
+end
 
-delay=20;
+function system_y1=y1(signal,t)
+    system_y1=2.*signal;
+end
 
-input_signal=signal_x1(t);
+function system_y2=y2(signal,t)
+    system_y2=signal.*signal;
+end
 
-shifted_input=zeros(length(t));
-
-for i=1:1:length(t)
-    k=i+delay;
-    if i<=length(t)
-        shifted_input(k)=input_signal(i);
+function system_y3=y3(signal,t)
+    system_y3=zeros(1,length(t));
+    for i=1:1:length(t)
+        system_y3(i)=t(i)*exp(signal(i));
     end
 end
 
-system_shifted_signal_output=y1(shifted_input);
-
-
-system_output=y1(input_signal);
-shifted_system_output=zeros(length(t));
-
-for i=1:1:length(t)
-    k=i+delay;
-    if i<=length(t)
-        shifted_system_output(k)=system_output(i);
+function delayed_signal=delay_signal(signal,delay,t)
+    delayed_signal=zeros(length(t));
+    for i=1:1:length(t)
+        k=i+delay;
+        if i<=length(t)
+            delayed_signal(k)=signal(i);
+        end
     end
 end
 
-figure;
-subplot(1,2,1);
-plot(t,system_shifted_signal_output);
-title("y1(x(t-T))");
-xlabel("Time (t)");
-ylabel("Amplitude")
+function time_variance=is_timeVariant(signal,system,t,delay,system_name,signal_name)
+    delayed_signal_system_outout=system(delay_signal(signal,delay,t),t);
+    shifted_system_output=delay_signal(system(signal,t),delay,t);
+   
+    figure;
+    subplot(2,3,1);
+    plot(t,signal);
+    title(sprintf("%s(t)",signal_name));
+    xlabel("Time (t)");
+    ylabel("Amplitude");
 
-subplot(1,2,2);
-plot(t,shifted_system_output);
-title("y1(x(t)-T)");
-xlabel("Time (t)");
-ylabel("Amplitude")
+    subplot(2,3,2);
+    plot(t,system(signal,t));
+    title(sprintf("%s(%s(t))",system_name,signal_name));
+    xlabel("Time (t)");
+    ylabel("Amplitude");
 
-if system_shifted_signal_output-shifted_system_output==0
-    disp("System is time invariant");
-else
-    disp("System is time variant");
+    subplot(2,3,3);
+    plot(t,shifted_system_output);
+    title(sprintf("%s(%s(t)-T)",system_name,signal_name));
+    xlabel("Time (t)");
+    ylabel("Amplitude");
+
+    subplot(2,3,4);
+    plot(t,signal);
+    title(sprintf("%s(t)",signal_name));
+    xlabel("Time (t)");
+    ylabel("Amplitude");
+
+    subplot(2,3,5);
+    plot(t,delay_signal(signal,delay,t));
+    title(sprintf("%s(t-T)",signal_name));
+    xlabel("Time (t)");
+    ylabel("Amplitude");
+
+    subplot(2,3,6);
+    plot(t,delayed_signal_system_outout);
+    title(sprintf("%s(%s(t-T))",system_name,signal_name));
+    xlabel("Time (t)");
+    ylabel("Amplitude");
+    
+    time_variance=delayed_signal_system_outout-shifted_system_output;
+    fprintf("Tested signal %s.\n",signal_name);
+    if time_variance==0
+        fprintf("System %s is time invariant\n",system_name);
+    else
+        fprintf("System %s is time variant\n",system_name);
+    end
 end
+
+
+
+delay=200;
+input_signal_x1=signal_x1(t);
+input_signal_x2=signal_x2(t);
+
+is_timeVariant(input_signal_x1,@y1,t,delay,"y1","x1");
+is_timeVariant(input_signal_x1,@y2,t,delay,"y2","x1");
+is_timeVariant(input_signal_x1,@y3,t,delay,"y3","x1");
+
+is_timeVariant(input_signal_x2,@y1,t,delay,"y1","x2");
+is_timeVariant(input_signal_x2,@y2,t,delay,"y2","x2");
+is_timeVariant(input_signal_x2,@y3,t,delay,"y3","x2");
+
